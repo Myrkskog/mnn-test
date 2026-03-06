4b579b274b5dccc7426366b196ae0dd023c54681
magoose <eattheevidence@gmail.com>
2026-03-06 11:53:59 -0500

    dynamic kernelVersionparsing logic / buildLinux style overrides into mobile-nixos.kernel-builderstructure

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -3,6 +3,7 @@
   fetchFromGitea,
   fetchpatch,
   stdenv,
+  lib,
   bc,
   bison,
   flex,
@@ -13,7 +14,7 @@
   zstd, # To compress kernel modules
   features ? [ ],
   ...
-}:
+}@args:
 
 let
   kernelSrc = fetchFromGitea {
@@ -23,6 +24,24 @@ let
     rev = "dc7b19cffd9ef96b28da5441061ff26dce1025a6";
     hash = "sha256-5DJ/G7I193TVnVn08dA0UYtzSf6g/Us6/ep8DCaFAdQ=";
   };
+
+  # Dynamically parse version from the Makefile
+
+  kernelVersion = rec {
+    file = "${kernelSrc}/Makefile";
+    version = lib.head (builtins.match ".+VERSION = ([0-9]+).+" (builtins.readFile file));
+    patchlevel = lib.head (builtins.match ".+PATCHLEVEL = ([0-9]+).+" (builtins.readFile file));
+    sublevel = lib.head (builtins.match ".+SUBLEVEL = ([0-9]+).+" (builtins.readFile file));
+    extraversion = lib.head (builtins.match ".+EXTRAVERSION = ([a-z0-9-]+).+" (builtins.readFile file));
+
+    string = "${version}.${patchlevel}.${sublevel}${
+      lib.optionalString (extraversion != "") extraversion
+    }";
+  };
+
+  # Target version string with suffix
+
+  targetVersion = "${kernelVersion.string}-sdm845";
 
   configfile = stdenv.mkDerivation {
     name = "sdm845-kernel-config";
@@ -66,9 +85,11 @@ let
   };
 in
 
-mobile-nixos.kernel-builder {
-  version = "6.19.0-rc4-next-20260106-sdm845";
-  modDirVersion = "6.19.0-rc4-next-20260106-sdm845";
+(mobile-nixos.kernel-builder {
+  #version = "6.19.0-rc4-next-20260106-sdm845";
+  version = targetVersion;
+  #modDirVersion = "6.19.0-rc4-next-20260106-sdm845";
+  modDirVersion = targetVersion;
   configfile = configfile;
   src = kernelSrc;
 
@@ -92,9 +113,20 @@ mobile-nixos.kernel-builder {
   isModular = true;
   isCompressed = "gz";
 
-  # Patch Makefile to match modDirVersion
+  # Match the EXTRAVERSION in Makefile to our target modDirVersion
+  # Overwrite the DTB Makefile as requested
   postUnpack = ''
     substituteInPlace $sourceRoot/Makefile \
-      --replace 'EXTRAVERSION = -rc4-next-20260106' 'EXTRAVERSION = -rc4-next-20260106-sdm845'
+      --replace 'EXTRAVERSION = ${kernelVersion.extraversion}' 'EXTRAVERSION = ${kernelVersion.extraversion}-sdm845'
+    cp ${./arch-arm64-boot-dts-sdm845-Makefile} $sourceRoot/arch/arm64/boot/dts/qcom/Makefile
   '';
-}
+
+  # Add the compiler flags
+}).overrideAttrs
+  (old: {
+
+    NIX_CFLAGS_COMPILE =
+      (old.NIX_CFLAGS_COMPILE or "")
+      + " -Wno-error=return-type -Wno-error=implicit-function-declaration -Wno-error=int-conversion";
+
+  })
